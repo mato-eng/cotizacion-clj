a01f4debad2c0e27ec306b5d53fdcb1253dab081
mato-gallardo <matias.gallardo@neofinancial.com>
2022-09-29 16:09:24 -0600

    1.0

--- src/core.clj
+++ src/core.clj
@@ -1,11 +1,27 @@
 (ns core
-  (:require [clojure.data.json :as json]))
+  (:gen-class)
+  (:require [clojure.data.json :as json]
+            [dotenv :refer [env]]))
 
-(def uri-dolar-ambito "https://mercados.ambito.com/dolar/informal/variacion")
+(def dolar-informal-link (env "DOLAR_INFORMAL"))
+(def dolar-turista-link (env "DOLAR_TURISTA"))
+(def cad-usd-link (env "DOLAR_CAD"))
 
-(defn main [opts]
-  (let [response (slurp uri-dolar-ambito)
-        values (json/read-str response :key-fn keyword)]
-    (println "COTIZACION")
-    (printf "Hoy %s compra: %s - venta: %s" (:fecha values) (:compra values) (:venta values))))
+(defn async-fetch-info [url]
+  (future (slurp url)))
 
+(defn async-parse-json [data]
+  (future (json/read-json @data)))
+
+
+(defn -main [& args]
+  (let [dolar-informal (async-fetch-info dolar-informal-link)
+        dolar-turista (async-fetch-info dolar-turista-link)
+        cad-usd (async-fetch-info cad-usd-link)
+        informal (async-parse-json dolar-informal)
+        turista-value (async-parse-json dolar-turista)
+        cad-usd-value (async-parse-json cad-usd)]
+    (printf "DOLAR INFORMAL %s compra: %s - venta: %s\n" (:fecha @informal) (:compra @informal) (:venta @informal))
+    (printf "DOLAR TURISTA %s compra: %s - venta: %s\n" (:fecha @turista-value) (:compra @turista-value) (:venta @turista-value))
+    (printf "DOLAR-CAD %s" (get-in (first (:observations @cad-usd-value)) [:FXUSDCAD :v]))
+    (println)))
